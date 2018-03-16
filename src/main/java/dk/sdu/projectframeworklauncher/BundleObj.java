eb8010d6bfd72412e0abec47ca1c0c7de03bd73b
Zebraged <chrih16@student.sdu.dk>
2018-03-16 13:10:00 +0100

    ny

---
+++ src/main/java/dk/sdu/projectframeworklauncher/BundleObj.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.projectframeworklauncher;
+
+import java.io.File;
+import java.util.ArrayList;
+import org.osgi.framework.Bundle;
+
+/**
+ *
+ * @author Chris
+ */
+public class BundleObj {
+    
+    private Bundle bundle;
+    private File file;
+    
+    public BundleObj(Bundle bundle, File file){
+        this.bundle = bundle;
+        this.file = file;
+    }
+    
+    @Override
+    public String toString(){
+        ArrayList<String> ss = new ArrayList<>();
+        String[] s = file.getAbsolutePath().split("\\\\");1
+        String s = 
+        return ;
+    }
+    
+}
--- src/main/java/dk/sdu/projectframeworklauncher/FXMLController.java
+++ src/main/java/dk/sdu/projectframeworklauncher/FXMLController.java
@@ -1,25 +1,89 @@
 package dk.sdu.projectframeworklauncher;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.ServiceLoader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.launch.Framework;
+import org.osgi.framework.launch.FrameworkFactory;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
+    private ListView<BundleObj> jfxListview;
+    //  private ArrayList<Bundle> boo = new ArrayList<>(); 
+    private ObservableList<BundleObj> obs = FXCollections.observableArrayList();
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Button btnuni;
+    @FXML
+    private Button btnins;
+    @FXML
+    private Button btnsta;
+    @FXML
+    private Button btnsto;
+    @FXML
+    private Label pathLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        try {
+            FrameworkFactory fwFactory
+                    = ServiceLoader.load(FrameworkFactory.class)
+                            .iterator().next();
+            Framework framework = fwFactory.newFramework(null);
+            framework.init();
+            BundleContext bndlCtxt = framework.getBundleContext();
+            File folder = new File(".");
+            for (File file : folder.listFiles()) {
+                if (file.getName().endsWith(".jar")) {
+                    Bundle bundle = bndlCtxt.getBundle();
+                    BundleObj bundleobj = new BundleObj(bundle, file);  //Bundle bundle = bndlCtxt.installBundle(file.toURI().toString());
+                    obs.add(bundleobj);
+                }
+            }
+            jfxListview.setItems(obs);
+
+            framework.start();
+        } catch (BundleException ex) {
+            Logger.getLogger(FXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void ocListview(MouseEvent event) {
+        pathLabel.setText(jfxListview.selectionModelProperty().getValue().getSelectedItem().toString());
+
+    }
+
+    @FXML
+    private void btnUninstall(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnInstall(ActionEvent event) {
+        BundleObj current = jfxListview.selectionModelProperty().getValue().getSelectedItem();
+
+    }
+
+    @FXML
+    private void btnStart(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnStop(ActionEvent event) {
+    }
 }
