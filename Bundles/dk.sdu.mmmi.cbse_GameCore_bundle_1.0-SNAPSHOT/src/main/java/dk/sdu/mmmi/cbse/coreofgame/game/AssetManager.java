42c68c1fdbb3eaa0b71ad31bdfa4d095de60b393
Marc226 <Marc.g.nielsen@gmail.com>
2018-03-20 13:16:09 +0100

    merge Marcv2

---
+++ Bundles/dk.sdu.mmmi.cbse_GameCore_bundle_1.0-SNAPSHOT/src/main/java/dk/sdu/mmmi/cbse/coreofgame/game/AssetManager.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.coreofgame.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.data.Asset;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Marcg
+ */
+public class AssetManager {
+
+    private SpriteBatch batch;
+    private World world;
+    private GameData data;
+    private List<Entity> entities;
+
+    public AssetManager(World world, GameData data) {
+        this.world = world;
+        this.data = data;
+
+        entities = (ArrayList<Entity>) world.getEntities();
+        this.batch = new SpriteBatch();
+        batch.begin();
+    }
+
+    public void loadImages() {
+        batch.begin();
+        for (Entity entity : entities) {
+        }
+
+    }
+
+}
