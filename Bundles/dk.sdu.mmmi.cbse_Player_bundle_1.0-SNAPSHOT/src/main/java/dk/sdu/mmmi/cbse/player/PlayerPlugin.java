1843ae98818796dd0bde80af70af2b8af1561e4c
raglu <raglu16@student.sdu.dk>
2018-03-22 22:25:39 +0100

    cleanup i guess

--- Bundles/dk.sdu.mmmi.cbse_Player_bundle_1.0-SNAPSHOT/src/main/java/dk/sdu/mmmi/cbse/player/PlayerPlugin.java
+++ Bundles/dk.sdu.mmmi.cbse_Player_bundle_1.0-SNAPSHOT/src/main/java/dk/sdu/mmmi/cbse/player/PlayerPlugin.java
@@ -3,6 +3,8 @@ package dk.sdu.mmmi.cbse.player;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.entityparts.MovingPart;
+import dk.sdu.mmmi.cbse.common.entityparts.PositionPart;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 
 public class PlayerPlugin implements IGamePluginService {
@@ -14,11 +16,21 @@ public class PlayerPlugin implements IGamePluginService {
 
     @Override
     public void start(GameData gameData, World world) {
-        Entity player = new Player();
+        Entity player = createPlayer(gameData);
         playerID = world.addEntity(player);
-        System.out.println("Player Bundle started!!");
     }
 
+    private Entity createPlayer(GameData gameData) {
+        Entity playerObject = new Player();
+
+        float x = gameData.getDisplayWidth() / 3;
+        float y = gameData.getDisplayHeight() / 3;
+        float radians = 3.1415f / 2;
+        playerObject.add(new PositionPart(x, y, radians));
+        
+        return playerObject;
+    }
+    
     @Override
     public void stop(GameData gameData, World world) {
         world.removeEntity(playerID);
