178b6b8c3e33f7bec4a161dbb7e639a03415dc37
Marc226 <Marc.g.nielsen@gmail.com>
2018-03-22 20:33:54 +0100

    Playermovement(temp)

--- Bundles/Player/src/main/java/dk/sdu/mmmi/cbse/player/PlayerProcess.java
+++ Bundles/Player/src/main/java/dk/sdu/mmmi/cbse/player/PlayerProcess.java
@@ -5,9 +5,12 @@
  */
 package dk.sdu.mmmi.cbse.player;
 
+import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.data.GameKeys;
+import dk.sdu.mmmi.cbse.common.entityparts.PositionPart;
 
 /**
  *
@@ -16,7 +19,18 @@ import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 public class PlayerProcess implements IEntityProcessingService{
 
     public void process(GameData gameData, World world) {
-        
+        for(Entity entity : world.getEntities(Player.class)){
+            PositionPart part = entity.getPart(PositionPart.class);
+            if(gameData.getKeys().isDown(GameKeys.UP) == true){
+                part.setY(part.getY() + 5);
+            } else if(gameData.getKeys().isDown(GameKeys.DOWN) == true){
+                part.setY(part.getY() - 5);
+            } else if(gameData.getKeys().isDown(GameKeys.LEFT) == true){
+                part.setX(part.getX() - 5);
+            } else if(gameData.getKeys().isDown(GameKeys.RIGHT) == true){
+                part.setX(part.getX() + 5);
+            } 
+        }
     }
     
 }
