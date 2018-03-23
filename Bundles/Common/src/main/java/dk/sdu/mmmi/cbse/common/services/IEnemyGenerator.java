2f2d2dfca3db9b282c9f7e199269327fe2be91cd
metalfist213 <silverleaves13@gmail.com>
2018-03-23 12:31:32 +0100

    Beginning of Level Generator
    Begun developing generator for maps.

---
+++ Bundles/Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEnemyGenerator.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.common.services;
+
+/**
+ *
+ * @author Mr. Kinder
+ */
+public interface IEnemyGenerator {
+    public void generate(String enemyIdentifier, int x, int y);
+}
---
+++ Bundles/LevelGenerator/src/main/java/dk/sdu/mmmi/cbse/levelgenerator/LevelGenerator.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dk.sdu.mmmi.cbse.levelgenerator;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author Mr. Kinder
+ */
+public class LevelGenerator {
+
+    private String path;
+    private String tileSetPath;
+    private String background;
+    private Command currentCommand;
+
+    public LevelGenerator(String path) {
+        this.path = path;
+    }
+    
+    public void generate() throws FileNotFoundException, IOException {
+        prepareAssets();
+        BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
+        String line;
+        while((line=reader.readLine())!=null) {
+            parse(line);
+        }
+    }
+    
+    private void prepareAssets() {
+        //Prepare backgrounds and things alike.
+    }
+    
+    private void parse(String line) {
+        if(line.endsWith(":")) {
+            currentCommand = Command.getCommand(line);
+        };
+        
+        switch(currentCommand) {
+            case MAP_SETTINGS:
+                
+                break;
+            case OBJECTS:
+                
+                break;
+                
+            case TILES:
+                
+                break;
+                
+            default:
+                System.out.println("The command is invalid!");
+                break;
+        }
+    }
+    
+}
+
+enum Command {
+    MAP_SETTINGS, TILES, OBJECTS;
+    
+    static Command getCommand(String cmdName) {
+        return Command.valueOf(cmdName.toUpperCase());
+    }
+}
