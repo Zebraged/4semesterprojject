425ad18d804ce9d3627eb292c332f9ae75f15649
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2018-03-27 18:16:26 +0200

    lame bug

--- Bundles/Map/src/main/java/MapServices/MapProcessor.java
+++ Bundles/Map/src/main/java/MapServices/MapProcessor.java
@@ -12,7 +12,7 @@ import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
-import dk.sdu.mmmi.cbse.map.MapServices;
+
 
 /**
  *
