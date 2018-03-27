b5412fad10fe43baae214cbfb114a683a969ddd0
metalfist213 <silverleaves13@gmail.com>
2018-03-27 09:50:41 +0200

    ---
    Added interface and fixed some bugs.

--- Bundles/LevelGenerator/src/main/java/dk/sdu/mmmi/cbse/levelgenerator/LevelGenerator.java
+++ Bundles/LevelGenerator/src/main/java/dk/sdu/mmmi/cbse/levelgenerator/LevelGenerator.java
@@ -21,53 +21,59 @@ public class LevelGenerator {
     private String path;
     private String tileSetPath;
     private String background;
-    private Command currentCommand;
+    private Command currentCommand = Command.NONE;
 
     public LevelGenerator(String path) {
         this.path = path;
     }
-    
+
     public void generate() throws FileNotFoundException, IOException {
         prepareAssets();
         BufferedReader reader = new BufferedReader(new FileReader(new File(path)));
         String line;
-        while((line=reader.readLine())!=null) {
+        while ((line = reader.readLine()) != null) {
+            //Go to next line if line is empty.
+            if (line.length() == 0) {
+                continue;
+            }
+
             parse(line);
         }
     }
-    
+
     private void prepareAssets() {
         //Prepare backgrounds and things alike.
     }
-    
+
     private void parse(String line) {
-        if(line.endsWith(":")) {
-            currentCommand = Command.getCommand(line);
-        };
-        
-        switch(currentCommand) {
+        if (line.endsWith(":")) {
+            currentCommand = Command.getCommand(line.substring(0, line.length()-1));
+        }
+
+        switch (currentCommand) {
             case MAP_SETTINGS:
-                
                 break;
             case OBJECTS:
-                
                 break;
-                
+
             case TILES:
-                
                 break;
-                
+
             default:
                 System.out.println("The command is invalid!");
                 break;
         }
     }
-    
+
+    public static void main(String[] args) throws IOException {
+        LevelGenerator generator = new LevelGenerator("map_example.lvl");
+        generator.generate();
+    }
 }
 
 enum Command {
-    MAP_SETTINGS, TILES, OBJECTS;
-    
+    NONE, MAP_SETTINGS, TILES, OBJECTS;
+
     static Command getCommand(String cmdName) {
         return Command.valueOf(cmdName.toUpperCase());
     }
