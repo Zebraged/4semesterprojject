53ef361f8763900be9c8987ceffeb0d44022baa6
Zebraged <chrih16@student.sdu.dk>
2018-04-03 12:14:30 +0200

    IenTITTYImple
    now with interface (Y)

--- Bundles/Platform/src/main/java/dk/sdu/mmmi/cbse/platform/Activator.java
+++ Bundles/Platform/src/main/java/dk/sdu/mmmi/cbse/platform/Activator.java
@@ -1,5 +1,6 @@
 package dk.sdu.mmmi.cbse.platform;
 
+import dk.sdu.mmmi.cbse.common.services.IEntityGenerator;
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 import org.osgi.framework.BundleActivator;
@@ -7,10 +8,12 @@ import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
     private IGamePluginService plugin = new PlatformPlugin();
+    private IEntityGenerator factory = new PlatformFactory();
     //private IEntityProcessingService process = new PlatformProcess();
 
     public void start(BundleContext context) throws Exception {
         context.registerService(IGamePluginService.class.getName(), plugin, null);
+        context.registerService(IEntityGenerator.class.getName(), factory, null);
         //context.registerService(IEntityProcessingService.class.getName(), process, null);
     }
 
