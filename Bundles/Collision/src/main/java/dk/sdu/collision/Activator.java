cecccf55b56522f096738e7a7a071ded58a45cf4
Jesper Kragh Jørgensen <jejoe16@student.sdu.dk>
2018-04-03 22:50:36 +0200

    lookup

--- Bundles/Collision/src/main/java/dk/sdu/collision/Activator.java
+++ Bundles/Collision/src/main/java/dk/sdu/collision/Activator.java
@@ -1,17 +1,32 @@
 package dk.sdu.collision;
 
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 public class Activator implements BundleActivator {
 
+    private List<Bundle> activatedBundles = new ArrayList();
+    
     public void start(BundleContext context) throws Exception {
 
         Collision collision = new Collision();
+        
+        activatedBundles = Arrays.asList(context.getBundles()); // get all installed bundles 
 
-        System.out.println(Arrays.toString(context.getBundles()));
+        for (Bundle b : activatedBundles){
+            
+            b.getBundleContext().getServiceReference(IGamePluginService.class.getName());
+            
+        }
+        
+        
+       // System.out.println(Arrays.toString(context.getBundles()));
         
         context.registerService(IEntityProcessingService.class.getName(), collision, null);
 
